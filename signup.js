c10eb5155d3fce542fbaa274b49e4c63cfb30f52
Makinde Mayowa <mayowamakinde23@gmail.com>
2023-12-07 05:34:22 +0100

    Initial commit

---
+++ signup.js
@@ -0,0 +1,50 @@
+const username = document.querySelector('#username')
+const email = document.querySelector('#email')
+const password = document.querySelector('#password')
+const signupform = document.querySelector('#signupform')
+const users = []
+const allusers = JSON.parse(localStorage.getItem('users')) || [];
+
+signupform.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    if(email.value=='' || username.value == '' || password.value==''){
+        alert('ensure that all fields are filled correctly')
+        return
+    }
+    const signupFormat={
+        id:email.value,
+        username: username.value,
+        password: password.value
+    }
+    const existingUser = allusers.map(user=>{
+        return user
+    }).filter(user=>{
+        return user.id == email.value
+    })[0]
+    console.log(existingUser)
+    if(existingUser){
+        alert('This email is already in use')
+        return
+    }else{
+        users.push(signupFormat);
+        update();
+        emptyInputs()
+    }
+    alert('Your account was created successfully')
+    location.replace('index.html')
+})
+
+function update(){
+    localStorage.setItem('users',JSON.stringify(users))
+}
+function emptyInputs(){
+    username.value = '';
+    email.value = '';
+    password.value = '';
+}
+
+const letters = ['a','b','c','d']
+
+function encryptPassword(pass){
+
+}
